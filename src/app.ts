b98bc7520b76fd2f41ee4ab9db970b31f1aeddc7
Mauricio Fullana <mfullana4@gmail.com>
2022-01-03 20:30:55 -0300

    QA config variables added

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,21 @@ import connect from "./utils/connect";
 import logger from "./utils/logger";
 import routes from "./routes";
 
-const port = config.get<number>("port");
+const port = config.get<number | null>("port");
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(port, async () => {
+const listener = async () => {
   logger.info(`App is running http://localhost:${port}`);
 
   await connect();
   routes(app);
-});
+};
+
+if (port) {
+  app.listen(port, listener);
+} else {
+  app.listen(listener);
+}
